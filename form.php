046e9976ec24582ee7d5fc5da2940e38735e510f
FSitnikov1 <mr.prizrek@mail.ru>
2022-05-22 20:26:05 +0300

    for 4

--- form.php
+++ form.php
@@ -1,72 +1,231 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-if (!empty($_GET['save'])){
-    echo'<script>alert("Результаты сохранены!");</script>';
-}
-include('index.php');
-exit();
-}
+<!DOCTYPE html>
 
-$errors = FALSE;
-if(empty($_POST['username'])){
-    print('Заполните поле имя.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['email'])){
-    print('Заполните поле E-mail.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['birthday'])){
-    print('Выберите дату рождения.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['sex'])){
-    print('Выберите пол.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['limbs'])){
-    print('Выберите количество конечностей.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['superpowers'])){
-    print('Выберите сверхспособности.<br/>');
-    $errors = TRUE;
-}
-if(empty($_POST['biography'])){
-    $_POST['biography']=" ";
-}
-if($errors){
-    print('Попробуйте ещё раз.');
-    exit();
-}
+<head>
+    <!-- Настройка viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Название веб-страницы во вкладках -->
+    <title>DATABASE_Form</title>
+    <style>
+        body {
+            background-color: darkgrey;
+        }
 
-$user = 'u46491';
-$pass = '2600028';
+        .main {
+            margin: auto;
+        }
 
-try {
-    $db = new mysqli("localhost", "u46491", "2600028", "u46491");
-} catch (PDOException $e) {
-	die($e->getMessage());
-}
+        .formula {
+            display: flex;
+            flex-direction: column;
+            margin: 50px auto;
+            padding: 20px;
+            max-width: 400px;
+            align-items: center;
+            background-color: gray;
+            border: solid 4px rgb(58, 53, 53);
+            border-radius: 15%;
+        }
 
-$name = $_POST['username'];
-$email = $_POST['email'];
-$birthday = $_POST['birthday'];
-$sex = $_POST['sex'];
-$limbs = $_POST['limbs'];
-$biography = $_POST['biography'];
-$superpowers = $_POST['superpowers'];
+        h1 {
+            color: rgb(35, 32, 32);
+        }
 
+        a:link {
+            color: rgba(38, 38, 63, 0.67);
+        }
 
+        a:visited {
+            color: rgba(116, 30, 196, 0.45);
+        }
 
-$db->query("SET NAMES 'utf8'");
-$db->query("INSERT INTO `form` (`name`, `email`, `birthday`, `sex`, `limbs`, `biography`) VALUES ('$name', '$email', '$birthday', '$sex', '$limbs', '$biography')");
-$db->query("INSERT INTO `super` (`superpowers`) VALUES ('$superpowers')");
-$db->close();
-if ($db->connect_error){
-    echo "Error Number: ".$db->connect_errno."<br>";
-    echo "Error: ".$db->connect_error;
-}
+        .form_item {
+            margin: 5px;
+            padding: 5px;
+            font-size: 20px;
+            font-weight: 1000;
+        }
 
-header('Location: ?save=1');
-?>
+        .form_label {
+            margin: 2px;
+            padding: 2px;
+            font-size: 18px;
+            font-weight: 800;
+        }
+
+        .formula textarea {
+            min-height: 100px;
+            resize: vertical;
+        }
+
+        .formula input,
+        textarea {
+            border: 2px solid rgba(0, 0, 0, 0.767);
+            border-radius: 5px;
+            font-size: 15px;
+            font-weight: 600;
+        }
+
+        .btn {
+            width: 200px;
+        }
+
+        .form_button {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 40px;
+            background-color: rgba(45, 173, 45, 0.432);
+            color: beige;
+            font-size: 15px;
+            font-weight: 600;
+            text-transform: uppercase;
+            letter-spacing: 2px;
+            cursor: pointer;
+            border-radius: 15px;
+            box-shadow: 0 2px 0 black;
+        }
+
+        .error {
+            border: 2px solid red;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    if (!empty($messages)) {
+        print('<div id="messages">');
+        foreach ($messages as $message) {
+            print($message);
+        }
+        print('</div>');
+    }
+    ?>
+
+    <div class="main">
+        <form class="formula" action="" method="POST">
+            <h2 id="forma" class="undershape">Форма</h2>
+            <div class="form_item <?php if ($errors['fio']) {
+                                        print 'error';
+                                    } ?>">
+                <label class="form_label labe">
+                    Имя:<br>
+                    <input type="text" name="fio" value="<?php print $values['fio']; ?>">
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['email']) {
+                                        print 'error';
+                                    } ?>">
+                <label class="form_label labe">
+                    E-mail:<br>
+                    <input type="email" name="email" value="<?php print $values['email']; ?>" <?php if ($errors['email']) {
+                                                                                                    print 'class="error"';
+                                                                                                } ?>>
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['birthday']) {
+                                        print 'error';
+                                    } ?>">
+                <label class="form_label labe">
+                    Дата рождения:<br>
+                    <input type="date" name="birthday" value="<?php print $values['birthday']; ?>" <?php if ($errors['birthday']) {
+                                                                                                        print 'class="error"';
+                                                                                                    } ?>>
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['sex']) {
+                                        print 'error';
+                                    } ?>">
+                <br>Пол:
+                <label class="form_label labe">
+                    <input type="radio" name="sex" value=1 <?php if ($values['sex'] == '1') {
+                                                                print 'checked="checked"';
+                                                            } ?>>
+                    женский
+                </label>
+                <label class="form_label labe">
+                    <input type="radio" name="sex" value=2 <?php if ($values['sex'] == '2') {
+                                                                print 'checked="checked"';
+                                                            } ?>>
+                    мужской
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['limbs']) {
+                                        print 'error';
+                                    } ?>">
+                <br>Количество конечностей:<br>
+                <label class="form_label">
+                    <input type="radio" name="limbs" value=1 <?php if ($values['limbs'] == 1) {
+                                                                    print 'checked="checked"';
+                                                                } ?>>
+                    1
+                </label>
+                <label class="form_label">
+                    <input type="radio" name="limbs" value=2 <?php if ($values['limbs'] == 2) {
+                                                                    print 'checked="checked"';
+                                                                } ?>>
+                    2
+                </label>
+                <label class="form_label">
+                    <input type="radio" name="limbs" value=3 <?php if ($values['limbs'] == 3) {
+                                                                    print 'checked="checked"';
+                                                                } ?>>
+                    3
+                </label>
+                <label class="form_label">
+                    <input type="radio" name="limbs" value=4 <?php if ($values['limbs'] == 4 or empty($values['limbs'])) {
+                                                                    print 'checked="checked"';
+                                                                } ?>>
+                    4
+                </label>
+                <label class="form_label">
+                    <input type="radio" name="limbs" value=5 <?php if ($values['limbs'] == 5) {
+                                                                    print 'checked="checked"';
+                                                                } ?>>
+                    5
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['superpowers']) {
+                                        print 'error';
+                                    } ?>">
+                Сверхспособности:<br>
+                <label class="form_label labe">
+                    <select name="superpowers">
+                        <option value=1 <?php if ($values['superpowers'] == 1) {
+                                            print 'selected="selected"';
+                                        } ?>>Бессмертие</option>
+                        <option value=2 <?php if ($values['superpowers'] == 2) {
+                                            print 'selected="selected"';
+                                        } ?>>Прохождение сквозь стены</option>
+                        <option value=3 <?php if ($values['superpowers'] == 3) {
+                                            print 'selected="selected"';
+                                        } ?>>Левитация</option>
+                    </select>
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['biography']) {
+                                        print ' error';
+                                    } ?>">
+                Биография: <br>
+                <label class="form_label labe">
+                    <textarea name="biography" autofocus><?php print $values['biography']; ?></textarea>
+                </label>
+            </div>
+            <div class="form_item <?php if ($errors['check']) {
+                                        print 'error';
+                                    } ?>">
+                <label>
+                    <input type="checkbox" name="check" value=1 <?php if ($values['check']) {
+                                                                    print 'checked="checked"';
+                                                                } ?>> с контрактом ознакомлен (а) <br>
+                </label>
+            </div>
+            <div class="form_item btn">
+                <input class="form_button" type="submit" «Отправить»>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,99 +1,217 @@
-<!DOCTYPE html>
-<html lang="Ru">
+<?php
 
-<head>
-    <!-- Кодировка веб-страницы -->
-    <meta charset="UTF-8">
-    <!-- Настройка viewport -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Подключение css файла -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Название веб-страницы во вкладках -->
-    <title>DATABASE_Form</title>
-</head>
+//  Отправляем браузеру кодировку
+header('Content-Type: text/html; charset=UTF-8');
+setlocale(LC_ALL, "ru_RU.UTF-8");
 
-<body>
-    <div class="main">
-        <form class="formula" action="form.php" method="POST">
-            <h2 id="forma" class="undershape">Форма</h2>
-            <div class="form_item">
-                <label class="form_label labe">
-                    Имя:<br>
-                    <input type="text" name="username">
-                </label>
-            </div>
-            <div class="form_item">
-                <label class="form_label labe">
-                    E-mail:<br>
-                    <input type="email" name="email">
-                </label>
-            </div>
-            <div class="form_item">
-                <label class="form_label labe">
-                    Дата рождения:<br>
-                    <input type="date" name="birthday">
-                </label>
-            </div>
-            <div class="form_item">
-                <br>Пол:
-                <label class="form_label labe">
-                    <input type="radio" name="sex" value=1>
-                    женский
-                </label>
-                <label class="form_label labe">
-                    <input type="radio" name="sex" value=2>
-                    мужской
-                </label>
-            </div>
-            <div class="form_item">
-                <br>Количество конечностей:<br>
-                <label class="form_label">
-                    <input type="radio" name="limbs" value=1>
-                    0
-                </label>
-                <label class="form_label">
-                    <input type="radio" name="limbs" value=2>
-                    1
-                </label>
-                <label class="form_label">
-                    <input type="radio" name="limbs" value=3>
-                    2
-                </label>
-                <label class="form_label">
-                    <input type="radio" name="limbs" value=4>
-                    3
-                </label>
-                <label class="form_label">
-                    <input type="radio" checked="checked" name="limbs" value=5>
-                    4
-                </label>
-            </div>
-            <div class="form_item">
-                Сверхспособности:<br>
-                <label class="form_label labe">
-                    <select name="superpowers" multiple="multiple">
-                        <option value=0 selected="selected">Бессмертие</option>
-                        <option value=1>Прохождение сквозь стены</option>
-                        <option value=2>Левитация</option>
-                    </select>
-                </label>
-            </div>
-            <div class="form_item">
-                Биография: <br>
-                <label class="form_label labe">
-                    <textarea name="biography" autofocus></textarea>
-                </label>
-            </div>
-            <div class="form_item">
-                <label>
-                    <input type="checkbox" name="checkbox" value = 1> с контрактом ознакомлен (а) <br>
-                </label>
-            </div>
-            <div class="form_item btn">
-                <input class="form_button" type="submit" «Отправить»>
-            </div>
-        </form>
-    </div>
-</body>
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    //  Массив для хранения сообщений пользователю
+    $messages = array();
+    // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
+    if (!empty($_COOKIE['save'])) {
+        setcookie('save', '', time() + 24 * 60 * 60);
+        $messages[] = 'Результаты были сохранены!';
+    }
+    //  Массив для хранения ошибок
+    $errors = array();
+    $errors['fio'] = !empty($_COOKIE['fio_error']);
+    $errors['email'] = !empty($_COOKIE['email_error']);
+    $errors['birthday'] = !empty($_COOKIE['birthday_error']);
+    $errors['sex'] = !empty($_COOKIE['sex_error']);
+    $errors['limbs'] = !empty($_COOKIE['limbs_error']);
+    $errors['superpowers'] = !empty($_COOKIE['superpowers_error']);
+    $errors['biography'] = !empty($_COOKIE['biography_error']);
+    $errors['check'] = !empty($_COOKIE['check_error']);
 
-</html>
+    //  Сообщения об ошибках
+    if ($errors['fio']) {
+        setcookie('fio_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Введите ФИО.</div>';
+    }
+    if ($errors['email']) {
+        setcookie('email_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Введите email.</div>';
+    }
+    if ($errors['birthday']) {
+        setcookie('birthday_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Выберите дату рождения.</div>';
+    }
+    if ($errors['sex']) {
+        setcookie('sex_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Выберите пол.</div>';
+    }
+    if ($errors['limbs']) {
+        setcookie('limbs_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Выберите кол-во конечностей.</div>';
+    }
+    if ($errors['superpowers']) {
+        setcookie('superpowers_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Выберите суперсилы.</div>';
+    }
+    /*
+    if ($errors['biography']) {
+        setcookie('biography_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Что-то пошло не так.</div>';
+    }
+    */
+    if ($errors['check']) {
+        setcookie('check_error', '', time() + 24 * 60 * 60);
+        $messages[] = '<div class="error">Согласитесь с условиями.</div>';
+    }
+
+    //  Сохраняем значения полей в массив
+    $values = array();
+    $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+    $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+    $values['birthday'] = empty($_COOKIE['birthday_value']) ? '' : $_COOKIE['birthday_value'];
+    $values['sex'] = empty($_COOKIE['sex_value']) ? '' : $_COOKIE['sex_value'];
+    $values['limbs'] = empty($_COOKIE['limbs_value']) ? '' : $_COOKIE['limbs_value'];
+    $values['superpowers'] = empty($_COOKIE['superpowers_value']) ? '' : $_COOKIE['superpowers_value'];
+    $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
+    $values['check'] = empty($_COOKIE['check_value']) ? '' : $_COOKIE['check_value'];
+
+    //  Включаем файл form.php
+    //  в него передаются переменные $messages, $errors, $values
+    include('form.php');
+} else {
+    //  Если метод был POST
+    //  Флаг для отлова ошибок полей
+    $errors = FALSE;
+    if (empty($_POST['fio'])) {
+        setcookie('fio_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^[a-zA-Zа-яёА-ЯЁ\s\-]+$/u', $_POST['fio'])) {
+            setcookie('fio_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('fio_value', $_POST['fio'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    if (empty($_POST['email'])) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/', $_POST['email'])) {
+            setcookie('email_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('email_value', $_POST['email'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    if (empty($_POST['birthday'])) {
+        setcookie('birthday_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^[1-2][0|9|8][0-9][0-9]-[0-1][0-9]-[0-3][0-9]+$/', $_POST['birthday'])) {
+            setcookie('birthday_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('birthday_value', $_POST['birthday'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    if (empty($_POST['sex'])) {
+        setcookie('sex_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^\d+$/', $_POST['sex'])) {
+            setcookie('sex_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('sex_value', $_POST['sex'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    if (empty($_POST['limbs'])) {
+        setcookie('limbs_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^\d+$/', $_POST['limbs'])) {
+            setcookie('limbs_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('limbs_value', $_POST['limbs'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    if (empty($_POST['superpowers'])) {
+        setcookie('superpowers_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^\d+$/', $_POST['superpowers'])) {
+            setcookie('superpowers_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            $asup = $_POST['superpowers'];
+            setcookie('superpowers_value', $_POST['superpowers'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+    /*
+    if (empty($_POST['biography'])) {
+        setcookie('biography_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {*/
+
+    setcookie('biography_value', $_POST['biography'], time() + 31 * 24 * 60 * 60);
+    /*
+    }
+    */
+    if (empty($_POST['check'])) {
+        setcookie('check_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        if (!preg_match('/^\d+$/', $_POST['check'])) {
+            setcookie('check_error', '2', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('check_value', $_POST['check'], time() + 31 * 24 * 60 * 60);
+        }
+    }
+
+
+    if ($errors) {
+        header('Location: index.php');
+        exit();
+    } else {
+        setcookie('fio_error', '', 100000);
+        setcookie('email_error', '', 100000);
+        setcookie('birthday_error', '', 100000);
+        setcookie('sex_error', '', 100000);
+        setcookie('limbs_error', '', 100000);
+        setcookie('superpowers_error', '', 100000);
+        setcookie('biography_error', '', 100000);
+        setcookie('check_error', '', 100000);
+    }
+    //*************************
+
+    $user = 'u46491';
+    $pass = '2600028';
+
+    try {
+        $db = new mysqli("localhost", "u46491", "2600028", "u46491");
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+
+    $name = $_POST['username'];
+    $email = $_POST['email'];
+    $birthday = $_POST['birthday'];
+    $sex = $_POST['sex'];
+    $limbs = $_POST['limbs'];
+    $biography = $_POST['biography'];
+    $superpowers = $_POST['superpowers'];
+
+
+
+    $db->query("SET NAMES 'utf8'");
+    $db->query("INSERT INTO `form` (`name`, `email`, `birthday`, `sex`, `limbs`, `biography`) VALUES ('$name', '$email', '$birthday', '$sex', '$limbs', '$biography');");
+    $db->query("INSERT INTO `super` (`superpowers`) VALUES ('$superpowers');");
+    $db->close();
+    if ($db->connect_error) {
+        echo "Error Number: " . $db->connect_errno . "<br>";
+        echo "Error: " . $db->connect_error;
+    }
+
+    //*************************
+
+    setcookie('save', '1');
+    header('Location: index.php');
+}
