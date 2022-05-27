727c8f8f35ec9f96c3f71dbbbb418bcb0e084774
FSitnikov1 <mr.prizrek@mail.ru>
2022-05-27 12:20:05 +0300

    7.1

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php
-$db = mysqli_connect('localhost', 'root', 'root');
+$db = mysqli_connect('localhost', 'u46491', '2600028');
 $select_db = mysqli_select_db($db, 'u46491');
--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 session_destroy();
 setcookie('PHPSESSID', '', 1000);
+foreach ($_COOKIE as $key => $value) setcookie($key, '', time() - 3600, '/');
 $_SERVER['PHP_AUTH_USER'] = '';
 $_SERVER['PHP_AUTH_PW'] = '';
 header('Location: index.php');
